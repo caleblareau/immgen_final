aa3293b85e3301c73e66bfe6eb5af809fcac0d5c
caleblareau <caleblareau@g.harvard.edu>
2017-08-22 16:08:37 -0400

    gwas analysis

---
+++ code/23aug2017_GWASbuild.R
@@ -0,0 +1,31 @@
+library(data.table)
+library(GenomicRanges)
+library(diffloop)
+library(magrittr)
+
+keepers <- fread("../data/immgen_good_peaks.txt", header = FALSE)[[1]]
+lo <- fread("../liftover/ImmGen_hg19peaks.bed")
+lo <- lo[lo[["V4"]] %in% keepers,]
+lo$start <- lo$V2 - 125
+lo$end <- lo$V2 + 125
+
+lo_g <- makeGRangesFromDataFrame(lo, seqnames = "V1", start.field = "start",
+                                 end.field = "end", keep.extra.columns = TRUE)
+
+makeHitMat <- function(file){
+  gwas_g <- makeGRangesFromDataFrame(fread(file), seqnames = "V1", start.field = "V2", end.field = "V3")
+  as.numeric(1:length(lo_g) %in% queryHits(findOverlaps(lo_g, gwas_g)))
+}
+
+files <- list.files("../gwasRaw", full.names = TRUE)
+names <- gsub("../gwasRaw/", "", files) %>% gsub(pattern = "_pruned_rsq_0.8.bed", replacement = "")
+
+hitMat250 <- sapply(files, makeHitMat)
+colnames(hitMat250) <- names
+hitMat250 <- Matrix::Matrix(hitMat250[,colSums(hitMat250) >= 50], sparse = TRUE)
+saveRDS(hitMat250, "../gwas_filtered/hitMat250_filt50.rds")
+
+
+write.table(lo[["V4"]], file = "../gwas_filtered/ImmGenPeaksKeep.txt", sep = "\t",
+            quote = FALSE, col.names = FALSE, row.names = FALSE)
+
---
+++ code/23aug_chromVAR_gwas.R
@@ -0,0 +1,50 @@
+#+ echo=FALSE, message=FALSE, warning=FALSE
+library(data.table)
+library(chromVAR)
+library(SummarizedExperiment)
+library(Matrix)
+library(BSgenome.Mmusculus.UCSC.mm10)
+library(motifmatchr)
+library(chromVARmotifs)
+library(GenomicRanges)
+library(BuenColors)
+
+# For laptop
+BiocParallel::register(BiocParallel::MulticoreParam(2, progressbar = FALSE))
+
+counts <- data.matrix(fread("zcat < ../data/17aug_commonATAC_normalized.txt.gz"))
+peaksdf <- fread("../data/ImmGenATAC1219.peak.bed")
+peaks <- makeGRangesFromDataFrame(peaksdf, seqnames = "V1", start.field = "V2", end.field = "V3")
+keepers <- fread("../gwas_filtered/ImmGenPeaksKeep.txt", header = FALSE)[["V1"]]
+
+counts <- SummarizedExperiment(assays = list(counts = counts[peaksdf[["V4"]] %in% keepers,]),
+                               rowData = peaks[peaksdf[["V4"]] %in% keepers,], 
+                               colData = DataFrame(names = colnames(counts)))
+
+counts <- addGCBias(counts,  genome = BSgenome.Mmusculus.UCSC.mm10)
+gwas_ix <- readRDS("../gwas_filtered/hitMat250_filt50.rds")
+inblood <- gsub("-", "_", read.table("../gwas_filtered/enrichedInBlood_corces.txt", header = FALSE)[,1])
+dev <- computeDeviations(object = counts, annotations = gwas_ix)
+zscores <- assays(dev)[["z"]]
+zscores[zscores > 3] <- 3
+zscores[zscores < -3] <- -3
+heatmaply::heatmaply(zscores, colors = jdb_palette("brewer_spectra"))
+
+heatplot <- ggplot(reshape2::melt(zscores), aes(x=Var2, y=Var1)) +
+                geom_tile(aes(fill = value),colour = "white") + theme_bw() +
+                theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                      axis.text = element_text(colour = "black", family = "Helvetica", size = 10)) +
+                 coord_fixed(ratio=1) + scale_fill_gradientn(colors = jdb_palette("brewer_spectra")) +
+                theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+                theme(axis.title.x=element_blank(), axis.title.y=element_blank())
+ggsave(heatplot, filename = "../figures/22Aug_gwasEnrichment.pdf", width = 30, height = 45)
+
+odf <- data.frame(trait = rownames(assays(dev)[["z"]]),
+                  blood_enriched = rownames(assays(dev)[["z"]]) %in% inblood,
+                  assays(dev)[["z"]])
+
+write.table(odf, file = "../gwas_filtered/23aug_chromVAR.tsv", sep = "\t", row.names = FALSE, col.names = TRUE,
+            quote = FALSE)
+
+
+
